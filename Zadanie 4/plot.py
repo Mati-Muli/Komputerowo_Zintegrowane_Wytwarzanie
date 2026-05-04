c900c5091a251cb242ddbc51f577f2d17338f30d
Iza <280138@student.pwr.edu.pl>
2026-05-04 15:25:26 +0200

    poprawki

--- Zadanie 4/plot.py
+++ Zadanie 4/plot.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
-def plot_gantt(sequence, title="Wykres Gantta"):
-    fig, ax = plt.subplots(figsize=(10, 3))
+def plot_gantt(sequence, ax, title="Wykres Gantta"):
     current_time = 0
 
     for i, task in enumerate(sequence):
@@ -10,9 +9,14 @@ def plot_gantt(sequence, title="Wykres Gantta"):
         is_tardy = finish_time > task.d
 
         color = 'red' if is_tardy else 'skyblue'
-        ax.broken_barh([(start_time, task.p)], (10, 9), facecolors=color, edgecolor='black')
-        ax.text(start_time + task.p / 2, 14.5, f"T{task.id}", ha='center', va='center', fontweight='bold')
-        ax.text(start_time + task.p / 2, 11, f"d:{task.d}", ha='center', va='center', fontsize=8)
+        ax.broken_barh([(start_time, task.p)], (10, 9),
+                       facecolors=color, edgecolor='black')
+
+        ax.text(start_time + task.p / 2, 14.5,
+                f"T{task.id}", ha='center', va='center', fontweight='bold')
+
+        ax.text(start_time + task.p / 2, 11,
+                f"d:{task.d}", ha='center', va='center', fontsize=8)
 
         current_time = finish_time
 
@@ -22,5 +26,3 @@ def plot_gantt(sequence, title="Wykres Gantta"):
     ax.set_yticks([])
     ax.set_title(title)
     ax.grid(True, axis='x', linestyle='--', alpha=0.6)
-    plt.tight_layout()
-    plt.show()
